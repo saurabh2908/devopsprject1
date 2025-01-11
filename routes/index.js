287e0ea6627a3cce991d29df1e4257751710ff01
saurabh2908 <2908aman@gmail.com>
2025-01-11 19:30:01 +0530

    update testcase

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is for testing purpose of jenkins and dock for creating image' });
+  res.send("Hello, World!")
 });
 
 module.exports = router;
