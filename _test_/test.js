624a7e51b66c51fc6eff880b929fda5561a83d15
saurabh2908 <2908aman@gmail.com>
2025-01-11 19:55:05 +0530

    failed test

--- _test_/test.js
+++ _test_/test.js
@@ -4,6 +4,6 @@ const app = require('../app');  // Assuming your Express app is in index.js
 describe('GET /', () => {
   it('should return "Hello, World!"', async () => {
     const response = await request(app).get('/');
-    expect(response.text).toBe('Hello, World!');
+    expect(response.text).toBe('Hello, World');
   });
 });
